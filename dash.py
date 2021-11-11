9b619c752680ab6410fd95b423ba776a46686dca
Quoc Khanh Ngo <nqkhanh1234@gmail.com>
2021-11-12 04:09:09 +1100

    Update dash.py

--- dash.py
+++ dash.py
@@ -21,7 +21,7 @@ application = Flask(__name__)
 application.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
+# add a rule when the page is accessed with a name appended to the sites
 # URL.
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
